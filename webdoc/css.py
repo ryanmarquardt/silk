a3c03c1a34eedeebedb22be041838a08797691c1
Ryan Marquardt <ryan.marquardt@gmail.com>
2012-05-04 09:04:04 -0700

    Added css property classes

--- webdoc/css.py
+++ webdoc/css.py
@@ -2,8 +2,8 @@
 
 >>> print Block(Selector('div'),
 ...     Property.new('padding')('0', Units.Px(20), Units.Px(15)),
-...     Property.new('background')('transparent'),
-...     Property.new('border')(Units.Px(1), 'solid', Colors.Black),
+...     BACKGROUND('transparent'),
+...     BORDER(Units.Px(1), 'solid', Colors.Black),
 ... )
 div { padding: 0 20px 15px; background: transparent; border: 1px solid #000000; }
 
@@ -28,6 +28,11 @@ Colors are stored with CamelCase names
 >>> Colors.from_rgb(255,255,255)
 '#FFFFFF'
 
+Classes for common properties exist also
+>>> print DISPLAY(None)
+display: none;
+>>> print BORDER.WIDTH(px=1)
+border-width: 1px;
 """
 
 from common import *
@@ -110,10 +115,6 @@ class Property(CSSNode):
 	def __str__(self):
 		return '%s: %s;' % (self.name, ' '.join(map(_css,self.children)))
 
-Attributes = container(
-	background = partial(Property, 'background')
-)
-
 __all__.append('Selector')
 class Selector(CSSNode):
 	'''Class for specifying a css element selector
@@ -171,58 +172,288 @@ Units = container(
 	S = lambda i:'%gs'%i,
 )
 
-### Factory functions for properties that accept units as arguments
-for name in """animation animation-name animation-duration animation-timing-function
-animation-delay animation-iteration-count animation-direction animation-play-state
-background background-attachment background-color background-image background-position
-background-repeat background-clip background-origin background-size
-border border-bottom border-bottom-color border-bottom-style border-bottom-width
-border-color border-left border-left-color border-left-style border-left-width
-border-right border-right-color border-right-style border-right-width border-style
-border-top border-top-color border-top-style border-top-width border-width
-border-radius border-bottom-left-radius border-bottom-right-radius
-border-top-left-radius border-top-right-radius
-border-image border-image-outset border-image-repeat border-image-slice border-image-source border-image-width
-outline outline-color outline-style outline-width
-box-decoration-break box-shadow
-overflow-x overflow-y overflow-style rotation rotation-point
-color-profile opacity rendering-intent
-bookmark-label bookmark-level bookmark-target float-offset
-hyphenate-after hyphenate-before hyphenate-character hyphenate-lines hyphenate-resource
-hyphens image-resolution marks string-set
-width height max-height min-height max-width min-width
-box-align box-direction box-flex box-flex-group box-lines box-ordinal-group box-orient box-pack
-font font-family font-size font-style font-variant font-weight font-size-adjust font-stretch
-content counter-increment counter-reset quotes crop move-to page-policy
-grid-columns grid-rows target target-name target-new target-position
-alignment-adjust alignment-baseline baseline-shift dominant-baseline
-drop-initial-after-adjust drop-initial-after-align drop-initial-before-adjust drop initial-before-align
-drop-initial-size drop-initial-value inline-box-align text-height
-line-stacking line-stacking-ruby line-stacking-shift line-stacking-strategy
-list-style list-style-image list-style-position list-style-type
-margin margin-left margin-right margin-top margin-bottom
-marquee-direction marquee-play-count marquee-speed marquee-style
-column-count column-fill column-gap column-rule column-rule-color column-rule-style
-column-rule-width column-span column-width columns
-padding padding-left padding-right padding-top padding-bottom
-fit fit-position image-orientation page size
-bottom clear clip cursor display float left overflow position right top visibility z-index
-orphans page-break-after page-break-before page-break-inside widows
-ruby-align ruby-overhang ruby-position ruby-span
-mark mark-after mark-before phonemes rest rest-after rest-before
-voice-balance voice-duration voice-pitch voice-pitch-range voice-rate voice-stress voice-volume
-border-collapse border-spacing caption-side empty-cells table-layout
-color direction letter-spacing line-height text-align text-decoration text-indent text-transform
-unicode-bidi vertical-align white-space word-spacing hanging-punctuation punctuation-trim
-text-align-last text-justify text-outline text-overflow text-shadow text-wrap
-word-break word-wrap
-transform transform-origin transform-style perspective perspective-origin backface-visibility
-transition transition-property transition-duration transition-timing-function transition-delay
-appearance box-sizing icon nav-down nav-index nav-left nav-right nav-up outline-offset resize
-""".split():
-	pass
-	#globals()[name.upper().replace('-','_')] = partial(Property,name.lower().replace('_','-'))
-	#assert str(globals()[name.upper().replace('-','_')](0)) == '%s: 0;'%name.lower().replace('_','-'), str(globals()[name.upper()]())
+def newp(name):
+	return Property.new(name, name.upper().replace('-','_'))
+
+class ANIMATION(Property):
+	name = 'animation'
+	NAME = newp('animation-name')
+	DURATION = newp('animation-duration')
+	TIMING_FUNCTION = newp('animation-timing-function')
+	DELAY = newp('animation-delay')
+	ITERATION_COUNT = newp('animation-interation-count')
+	DIRECTION = newp('animation-direction')
+	PLAY_STATE = newp('animation-play-state')
+
+class BACKGROUND(Property):
+	name = 'background'
+	ATTACHMENT = newp('background-attachment')
+	COLOR = newp('background-color')
+	IMAGE = newp('background-image')
+	POSITION = newp('background-position')
+	REPEAT = newp('background-repeat')
+	CLIP = newp('background-clip')
+	ORIGIN = newp('background-origin')
+	SIZE = newp('background-size')
+
+class BORDER(Property):
+	name = 'border'
+	COLOR = newp('border-color')
+	STYLE = newp('border-style')
+	WIDTH = newp('border-width')
+	class TOP(Property):
+		name = 'border-top'
+		COLOR = newp('border-top-color')
+		STYLE = newp('border-top-style')
+		WIDTH = newp('border-top-width')
+	class RIGHT(Property):
+		name = 'border-right'
+		COLOR = newp('border-right-color')
+		STYLE = newp('border-right-style')
+		WIDTH = newp('border-right-width')
+	class BOTTOM(Property):
+		name = 'border-bottom'
+		COLOR = newp('border-bottom-color')
+		STYLE = newp('border-bottom-style')
+		WIDTH = newp('border-bottom-width')
+	class LEFT(Property):
+		name = 'border-left'
+		COLOR = newp('border-left-color')
+		STYLE = newp('border-left-style')
+		WIDTH = newp('border-left-width')
+	class RADIUS(Property):
+		name = 'border-radius'
+		BOTTOM_LEFT = newp('border-bottom-left-radius')
+		BOTTOM_RIGHT = newp('border-bottom-right-radius')
+		TOP_LEFT = newp('border-top-left-radius')
+		TOP_RIGHT = newp('border-top-right-radius')
+	class IMAGE(Property):
+		name = 'border-image'
+		OUTSET = newp('border-image-outset')
+		REPEAT = newp('border-image-repeat')
+		SLICE = newp('border-image-slice')
+		SOURCE = newp('border-image-source')
+		WIDTH = newp('border-image-width')
+
+class OUTLINE(Property):
+	name = 'outline'
+	COLOR = newp('outline-color')
+	STYLE = newp('outline-style')
+	WIDTH = newp('outline-width')
+	OFFSET = newp('outline-offset')
+
+class BOX_SHADOW(Property):
+	name = 'box-shadow'
+class OVERFLOW(Property):
+	name = 'overflow'
+	X = newp('overflow-x')
+	Y = newp('overflow-y')
+	STYLE = newp('overflow-style')
+class ROTATION(Property):
+	name = 'rotation'
+	POINT = newp('rotation-point')
+
+class OPACITY(Property):
+	name = 'opacity'
+class WIDTH(Property):
+	name = 'width'
+class MAX_WIDTH(Property):
+	name = 'max-width'
+class MIN_WIDTH(Property):
+	name = 'min-width'
+WIDTH.MAX = MAX_WIDTH
+WIDTH.MIN = MIN_WIDTH
+class HEIGHT(Property):
+	name = 'height'
+class MAX_HEIGHT(Property):
+	name = 'max-height'
+class MIN_HEIGHT(Property):
+	name = 'min-height'
+HEIGHT.MAX = MAX_HEIGHT
+HEIGHT.MIN = MIN_HEIGHT
+
+class BOX(container):
+	ALIGN = newp('box-align')
+	DIRECTION = newp('box-direction')
+	FLEX = newp('box-flex')
+	FLEX_GROUP = newp('box-flex-group')
+	LINES = newp('box-lines')
+	ORDINAL_GROUP = newp('box-ordinal-group')
+	ORIENT = newp('box-orient')
+	PACK = newp('box-pack')
+
+class FONT(Property):
+	FAMILY = newp('font-family')
+	SIZE = newp('font-size')
+	STYLE = newp('font-style')
+	VARIANT = newp('font-variant')
+	WEIGHT = newp('font-weight')
+	SIZE_ADJUST = newp('font-size-adjust')
+	STRETCH = newp('font-stretch')
+
+class CONTENT(Property):
+	name = 'content'
+class COUNTER(container):
+	INCREMENT = newp('counter-increment')
+	RESET = newp('counter-reset')
+	
+class QUOTES(Property):
+	name = 'quotes'
+class GRID(container):
+	COLUMNS = newp('grid-columns')
+	ROWS = newp('grid-rows')
+
+class TARGET(Property):
+	name = 'target'
+	NAME = newp('target-name')
+	NEW = newp('target-new')
+	POSITION = newp('target-position')
+
+class LIST_STYLE(Property):
+	name = 'list-style'
+	IMAGE = newp('list-style-image')
+	POSITION = newp('list-style-position')
+	TYPE = newp('list-style-type')
+
+class MARGIN(Property):
+	name = 'margin'
+	TOP = newp('margin-top')
+	RIGHT = newp('margin-right')
+	BOTTOM = newp('margin-bottom')
+	LEFT = newp('margin-left')
+
+class COLUMN(container):
+	COUNT = newp('column-count')
+	FILL = newp('column-fill')
+	GAP = newp('column-gap')
+	class RULE(Property):
+		name = 'column-rule'
+		COLOR = newp('column-rule-color')
+		STYLE = newp('column-rule-style')
+		WIDTH = newp('column-rule-width')
+	SPAN = newp('column-span')
+	WIDTH = newp('column-width')
+class COLUMNS(Property):
+	name = 'columns'
+
+class PADDING(Property):
+	name = 'padding'
+	TOP = newp('padding-top')
+	RIGHT = newp('padding-right')
+	BOTTOM = newp('padding-bottom')
+	LEFT = newp('padding-left')
+
+class TOP(Property):
+	name = 'top'
+class RIGHT(Property):
+	name = 'right'
+class BOTTOM(Property):
+	name = 'bottom'
+class LEFT(Property):
+	name = 'left'
+class CLEAR(Property):
+	name = 'clear'
+class CLIP(Property):
+	name = 'clip'
+class CURSOR(Property):
+	name = 'cursor'
+class DISPLAY(Property):
+	name = 'display'
+class FLOAT(Property):
+	name = 'float'
+class POSITION(Property):
+	name = 'position'
+class VISIBILITY(Property):
+	name = 'visibility'
+class Z_INDEX(Property):
+	name = 'z-index'
+
+class ORPHANS(Property):
+	name = 'orphans'
+class PAGE_BREAK(container):
+	AFTER = newp('page-break-after')
+	BEFORE = newp('page-break-before')
+	INSIDE = newp('page-break-inside')
+class WIDOWS(Property):
+	name = 'widows'
+
+class BORDER_COLLAPSE(Property):
+	name = 'border-collapse'
+class BORDER_SPACING(Property):
+	name = 'border-spacing'
+class CAPTION_SIDE(Property):
+	name = 'caption-side'
+class EMTPY_CELLS(Property):
+	name = 'empty-cells'
+class TABLE_LAYOUT(Property):
+	name = 'table-layout'
+	
+class COLOR(Property):
+	name = 'color'
+class DIRECTION(Property):
+	name = 'direction'
+class LETTER_SPACING(Property):
+	name = 'letter-spacing'
+class LINE_HEIGHT(Property):
+	name = 'line-height'
+class TEXT(container):
+	ALIGN = newp('text-align')
+	DECORATION = newp('text-decoration')
+	INDENT = newp('text-indent')
+	TRANSFORM = newp('text-transform')
+	ALIGN_LAST = newp('text-align-last')
+	JUSTIFY = newp('text-justify')
+	OUTLINE = newp('text-outline')
+	OVERFLOW = newp('text-overflow')
+	SHADOW = newp('text-shadow')
+	WRAP = newp('text-wrap')
+class VERTICAL_ALIGN(Property):
+	name = 'vertical-align'
+class WHITE_SPACE(Property):
+	name = 'white-space'
+class WORD_SPACING(Property):
+	name = 'word-spacing'
+class HANGING_PUNCTUATION(Property):
+	name = 'hanging-punctuation'
+class PUNCTUATION_TRIM(Property):
+	name = 'punctuation-trim'
+class WORD_BREAK(Property):
+	name = 'word-break'
+class WORD_WRAP(Property):
+	name = 'word-wrap'
+
+class TRANSFORM(Property):
+	name = 'transform'
+	ORIGIN = newp('transform-origin')
+	STYLE = newp('transform-style')
+class PERSPECTIVE(Property):
+	name = 'perspective'
+	ORIGIN = newp('perspective-origin')
+class BACKFACE_VISIBILITY(Property):
+	name = 'backface-visibility'
+class TRANSITION(Property):
+	name = 'transition'
+	PROPERTY = newp('transition-property')
+	DURATION = newp('transition-duration')
+	TIMING_FUNCTION = newp('transition-timing-function')
+	DELAY = newp('transition-delay')
+
+class APPEARANCE(Property):
+	name = 'appearance'
+class BOX_SIZING(Property):
+	name = 'box-sizing'
+class ICON(Property):
+	name = 'icon'
+class NAV(container):
+	DOWN = newp('nav-down')
+	INDEX = newp('nav-index')
+	LEFT = newp('nav-left')
+	RIGHT = newp('nav-right')
+	UP = newp('nav-up')
+class RESIZE(Property):
+	name = 'resize'
+
+del newp
 
 def css(**attributes):
 	return [globals()[name.upper().replace('-','_')](*sequence(value)) for name,value in attributes.items()]
