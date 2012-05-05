c1c206fbcce3e19febc11a6da8c29508ddee1598
Ryan Marquardt <ryan.marquardt@gmail.com>
2012-05-05 00:01:20 -0700

    Adding/removing rows and tables works

--- webdb/__init__.py
+++ webdb/__init__.py
@@ -1,10 +1,17 @@
 """
 
-Open a database connection. An in-memory database is created with
+
+Use the connect method to open a database connection.
+>>> mydb = DB.connect('sqlite','/tmp/webdb.doctest.sqlite')
+>>> import os
+>>> os.unlink('/tmp/webdb.doctest.sqlite')
+
+By default, an in-memory database is created.
 >>> mydb = DB()
 
-otherwise use the connect method
->>> mydb = DB.connect('sqlite','/tmp/webdb.doctest.sqlite')
+Currently only sqlite databases are supported. Other databases will be supported
+with drivers. See webdb.drivers documentation for more information.
+
 
 Tables are defined by assigning them to attributes of a database. Two special
 methods help with definitions.
@@ -12,24 +19,99 @@ methods help with definitions.
 Conform reads table definitions from the database, overriding any tables
 that have already been defined.
 >>> mydb.conform()
+>>> list(mydb) #No table definitions
+[]
 
->>> mydb.test_table = Table(Field('key'), Field('value'))
->>> mydb.test_table = Table(Field('key'), Field('value'), Field('extra'))
->>> mydb.test_table
-Table(Field('key', 'string'), Field('value', 'string'), Field('extra', 'string'))
+>>> mydb.test_table = Table(Column('key'), Column('value'))
+>>> list(mydb)
+[Table(Column('key', 'string'), Column('value', 'string'))]
+
+>>> mydb.test_table = Table(Column('key'), Column('value'), Column('extra'))
+>>> list(mydb)
+[Table(Column('extra', 'string'), Column('key', 'string'), Column('value', 'string'))]
+
 >>> mydb.conform()
->>> mydb.test_table
-Table(Field('key', 'string'), Field('value', 'string'))
->>> mydb.test_table = Table(Field('key'), Field('value'), Field('extra'))
+>>> list(mydb)
+[Table(Column('key', 'string'), Column('value', 'string'))]
 
 Migrate modifies tables in the database to be like newly-assigned tables.
+>>> mydb.test_table = Table(Column('key'), Column('value'), Column('extra'))
 >>> mydb.migrate()
 >>> mydb.test_table
-Table(Field('key', 'string'), Field('value', 'string'), Field('extra', 'string'))
+Table(Column('extra', 'string'), Column('key', 'string'), Column('value', 'string'))
+
+Conforming after a migration does nothing.
+>>> mydb.conform()
+>>> mydb.test_table
+Table(Column('extra', 'string'), Column('key', 'string'), Column('value', 'string'))
+
+Add some data by calling insert on a table. An integer referring to the new row
+is returned and can be used to retrieve it later.
+>>> i = mydb.test_table.insert(key='1', value='a')
+>>> row = mydb.test_table[i]
+>>> row.key
+'1'
+>>> row.value
+'a'
+>>> print row.extra
+None
+
 """
 import collections
 import inspect
 import drivers.sqlite
+
+from webdoc.common import container
+
+class collection(collections.MutableSet):
+	'''Set of objects which can also be retrieved by name
+
+	>>> class b(object):
+	...   def __init__(self, name, value):
+	...     self.name, self.value = name, value
+	...   def __repr__(self): return 'b(%r, %r)' % (self.name, self.value)
+	>>> a = collection()
+	>>> a.add(b('robert', 'Sys Admin'))
+	>>> a.add(b('josephine', 'Q/A'))
+	>>> a['robert']
+	b('robert', 'Sys Admin')
+	>>> sorted(list(a), key=lambda x:x.name)
+	[b('josephine', 'Q/A'), b('robert', 'Sys Admin')]
+	'''
+	def __init__(self, elements=(), namekey='name'):
+		self._key = namekey
+		self._data = dict((getattr(e,namekey),e) for e in elements)
+
+	def __len__(self):
+		return len(self._data)
+
+	def __iter__(self):
+		return self._data.itervalues()
+
+	def __contains__(self, value):
+		return getattr(value,self._key) in self._data
+
+	def add(self, value):
+		self._data[getattr(value,self._key)] = value
+
+	def discard(self, value):
+		del self._data[getattr(value,self._key)]
+
+	def keys(self):
+		return self._data.keys()
+
+	def __getitem__(self, key):
+		return self._data[key]
+
+	def __delitem__(self, key):
+		del self._data[key]
+
+	def pop(self, *item):
+		assert len(item) <= 1
+		if item:
+			return self._data.pop(item[0])
+		else:
+			return self._data.popitem()[1]
 
 class Row(object):
 	pass
@@ -40,8 +122,10 @@ class Selection(object):
 class Set(object):
 	pass
 
-class Field(object):
-	'''Field(name, type='string', notnull=False, default=None)
+ident = lambda x:x
+
+class Column(object):
+	'''Column(name, type='string', notnull=False, default=None)
 	
 	valid types:
 	'string' or basestring
@@ -51,7 +135,14 @@ class Field(object):
 	'''
 	def __init__(self, name, type='string', notnull=False, default=None):
 		self.name = name
-		self.type = type
+		if type == 'string':
+			self.type = 'string'
+			self.interpret = str
+			self.represent = str
+		else:
+			self.type = type
+			self.interpret = ident
+			self.represent = ident
 		self.notnull = notnull
 		self.default = default
 		self.index = None
@@ -63,59 +154,74 @@ class Field(object):
 			values.append('notnull=True')
 		if not self.default is None:
 			values.append('default=%r' % self.default)
-		return 'Field%i(%s)' % (self.index,', '.join(values))
+		return 'Column(%s)' % ', '.join(values)
 		
 	def __cmp__(self, x):
-		return cmp(self.index, x.index)
+		return cmp((self.name,vars(self)), (x.name,vars(self))) if x else 1
 
-class Table(object):
-	def __init__(self, *fields, **kwargs):
-		id_field = kwargs.get('id_field')
-		for i,f in enumerate(fields):
-			if f.index is None:
-				f.index = i+1
-			self.__dict__[f.name] = f
-			
-	def fields(self):
-		return vars(self).values()
-		
+class Table(collection):
+	def __init__(self, *columns, **kwargs):
+		collection.__init__(self, columns)
+
+	def __getattr__(self, key):
+		return collection.__getitem__(self, key)
+
+	def __delattr__(self, key):
+		collection.__delitem__(self, key)
+
+	def __getitem__(self, key):
+		names = self.keys()
+		columns = list(self)
+		values = self._db.__driver__.select(names, self._name, [('rowid',key)])[0]
+		return container((k,c.represent(v)) for k,v,c in zip(['rowid']+names,values,columns))
+
+	def __delitem__(self, key):
+		pass
+
+	def insert(self, **values):
+		return self._db.__driver__.insert(values, self._name)
+
 	def __repr__(self):
-		return 'Table(%s)' % ', '.join(map(repr,sorted(vars(self).values())))
+		return 'Table(%s)' % ', '.join(sorted(map(repr,self)))
 
-class DB(collections.Mapping):
+class DB(collection):
 	"""
 	
 	>>> mydb = DB.connect('sqlite')
-	>>> mydb.test = Table(Field('data'))
-	>>> list(mydb.tables())
-	[Table(Field('data', 'string'))]
+	>>> mydb.test = Table(Column('data'))
+	>>> list(mydb)
+	[Table(Column('data', 'string'))]
 	"""
 	__driver__ = drivers.sqlite.sqlite()
-	
-	def __setattr__(self, key, value):
-		#Create a table if it's new
-		assert isinstance(value, Table)
-		super(DB, self).__setattr__(key, value)
-		self.__driver__.create_table_if_nexists(key, value)
-		
-	def __getitem__(self, key):
-		return self.__dict__[key]
-		
-	def __setitem__(self, key, value):
-		self.__dict__[key] = value
-	
-	def __delitem__(self, key):
-		del self.__dict__[key]
 
-	def __iter__(self):
-		return iter(self.__dict__)
-		
-	def __len__(self):
-		return len(self.__dict__)
-		
-	def tables(self):
-		return self.values()
-		
+	def __init__(self):
+		collection.__init__(self, namekey='_name')
+	
+	def __setitem__(self, key, value):
+		assert isinstance(value, Table)
+		value._db = self
+		value._name = key
+		self.__driver__.create_table_if_nexists(key, value)
+		collection.add(self, value)
+
+	def __setattr__(self, key, value):
+		if key[0] == '_':
+			super(DB, self).__setattr__(key, value)
+		else:
+			self[key] = value
+
+	def __getattr__(self, key):
+		if key[0] == '_':
+			return super(DB, self).__getattr__(key)
+		else:
+			return self[key]
+	
+	def __delattr__(self, key):
+		if key[0] == '_':
+			super(DB, self).__delattr__(key)
+		else:
+			del self[key]
+
 	@classmethod
 	def connect(cls, name, *args, **kwargs):
 		driver = getattr(__import__('drivers.%s'%name, fromlist=name), name)
@@ -124,13 +230,23 @@ class DB(collections.Mapping):
 		
 	def conform(self):
 		for table in self.__driver__.list_tables():
-			fields = []
-			for idx,name,v_type,notnull,default in self.__driver__.list_columns(table):
-				fields.append(Field(name, v_type, notnull=notnull, default=default))
-			self.__dict__[table] = Table(*fields)
+			columns = []
+			for name,v_type,notnull,default in self.__driver__.list_columns(table):
+				columns.append(Column(name, v_type, notnull=notnull, default=default))
+			t = Table(*columns)
+			t._db = self
+			t._name = table
+			collection.add(self, t)
 		
 	def migrate(self):
-		pass
+		names = set(self.keys())
+		db_tables = set(self.__driver__.list_tables())
+		for name in names - db_tables:
+			#Create
+			self.__driver__.create_table(name, self[name])
+		for name in names.intersection(db_tables):
+			#Alter if not the same
+			self.__driver__.alter_table(name, self[name])
 
 if __name__=='__main__':
 	import doctest
--- webdb/drivers/sqlite.py
+++ webdb/drivers/sqlite.py
@@ -2,15 +2,29 @@
 from webdoc import container
 import sqlite3
 
+class driver_base(object):
+	def create_table_if_nexists(self, name, table):
+		if name not in self.list_tables:
+			self.create_table(name, table)
+
 class sqlite(object):
+	"""Driver for sqlite3 databases
+
+	sqlite accepts only one parameter: path, which is the path of the database
+	file. By default, path=':memory:', which creates a temporary database
+	in memory.
+	"""
 	def __init__(self, path=':memory:'):
 		self.path = path
 		self.connection = sqlite3.connect(path)
 		self.depth = 0
+		self.cursor = None
 		
 	def __enter__(self):
 		self.depth += 1
-		return self.connection.cursor()
+		if self.cursor is None:
+			self.cursor = self.connection.cursor()
+		return self.cursor
 		
 	def __exit__(self, obj, exc, tb):
 		self.depth -= 1
@@ -19,14 +33,17 @@ class sqlite(object):
 				self.connection.rollback()
 			else:
 				self.connection.commit()
+			self.cursor = None
+
+	def execute(self, sql, values=()):
+		with self as cursor:
+			return cursor.execute(sql, values)
 		
 	def list_tables(self):
-		with self as cursor:
-			return (n for (n,) in self.execute("""SELECT name FROM sqlite_master WHERE type='table'"""))
+		return (str(n) for (n,) in self.execute("""SELECT name FROM sqlite_master WHERE type='table'"""))
 		
 	def list_columns(self, table):
-		with self as cursor:
-			return sorted((idx,str(name),str(v_type),bool(notnull),default) for (idx,name,v_type,notnull,default,_) in self.execute("""PRAGMA table_info("%s");""" % table))
+		return ((str(name),str(v_type),bool(notnull),default) for (_,name,v_type,notnull,default,_) in self.execute("""PRAGMA table_info("%s");""" % table))
 			
 	@staticmethod
 	def format_value(val):
@@ -36,20 +53,48 @@ class sqlite(object):
 			return str(val)
 
 	@classmethod
-	def format_column(cls, field):
-		props = container(vars(field))
-		props.notnull = 'NOT NULL' if field.notnull else ''
-		props.default = 'DEFAULT %s'%cls.format_value(field.default) if field.notnull or not field.default is None else ''
+	def format_column(cls, column):
+		props = container(vars(column))
+		props.notnull = 'NOT NULL' if column.notnull else ''
+		props.default = "DEFAULT '%s'"%cls.format_value(column.default) if column.notnull or not column.default is None else ''
 		return '%(name)s %(type)s %(notnull)s %(default)s' % props
 
 	def create_table_if_nexists(self, name, table):
-		with self as cursor:
-			self.execute("""CREATE TABLE IF NOT EXISTS %s(%s);""" % (
-				name,
-				', '.join(map(self.format_column,table.fields()))
-			))
+		self.execute("""CREATE TABLE IF NOT EXISTS %s(%s);""" % (
+			name,
+			', '.join(map(self.format_column, table))
+		))
 
-	def execute(self, sql, values=()):
-		with self as cursor:
-			return cursor.execute(sql, values)
-		
+	def create_table(self, name, table):
+		self.execute("""CREATE TABLE %s(%s);""" % (
+			name,
+			', '.join(map(self.format_column,table))
+		))
+
+	def rename_table(self, orig, new):
+		self.execute("""ALTER TABLE %s RENAME TO %s;""" % (orig, new))
+
+	def alter_table(self, name, table):
+		with self:
+			db_cols = self.list_columns(name)
+			db_names = [c[0] for c in db_cols]
+			for column in table:
+				if column.name not in db_names:
+					self.execute("""ALTER TABLE %s ADD COLUMN %s;"""%(
+						name, self.format_column(column)
+					))
+
+	def select(self, columns, table, conditions):
+		return self.execute("""SELECT %s FROM %s WHERE %s""" % (
+			', '.join(['rowid'] + columns),
+			table,
+			' and '.join("%s=%s"%i for i in conditions),
+		)).fetchall()
+
+	def insert(self, values, table):
+		cur = self.execute("""INSERT INTO %s(%s) VALUES (%s)""" % (
+			table,
+			','.join(values.keys()),
+			','.join(list('?'*len(values))),
+		), values.values())
+		return cur.lastrowid
