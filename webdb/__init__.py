b42ff911aec670e5f1412dd3fc53314c678013f5
Ryan Marquardt <ryan.marquardt@gmail.com>
2012-05-16 17:20:05 -0700

    Added querying

--- webdb/__init__.py
+++ webdb/__init__.py
@@ -1,10 +1,10 @@
-"""
-
+"""Database abstraction layer
 
 Use the connect method to open a database connection.
->>> mydb = DB.connect('sqlite','/tmp/webdb.doctest.sqlite')
->>> import os
->>> os.unlink('/tmp/webdb.doctest.sqlite')
+>>> mydb = DB.connect('sqlite','path/to/database.sqlite')
+Traceback (most recent call last):
+ ...
+IOError: [Errno 2] No such file or directory: 'path/to/database.sqlite'
 
 By default, an in-memory database is created.
 >>> mydb = DB()
@@ -12,6 +12,9 @@ By default, an in-memory database is created.
 Currently only sqlite databases are supported. Other databases will be supported
 with drivers. See webdb.drivers documentation for more information.
 
+===
+Tables
+===
 
 Tables are defined by assigning them to attributes of a database. Two special
 methods help with definitions.
@@ -19,7 +22,7 @@ methods help with definitions.
 Conform reads table definitions from the database, overriding any tables
 that have already been defined.
 >>> mydb.conform()
->>> list(mydb) #No table definitions
+>>> list(mydb) #No tables defined yet
 []
 
 >>> mydb.test_table = Table(Column('key'), Column('value'))
@@ -35,35 +38,123 @@ that have already been defined.
 [Table(Column('key', 'string'), Column('value', 'string'))]
 
 Migrate modifies tables in the database to be like newly-assigned tables.
->>> mydb.test_table = Table(Column('key'), Column('value'), Column('extra'))
+>>> mydb.test_table = Table(Column('key', 'integer'), Column('value'), Column('extra'))
 >>> mydb.migrate()
 >>> mydb.test_table
-Table(Column('extra', 'string'), Column('key', 'string'), Column('value', 'string'))
+Table(Column('extra', 'string'), Column('key', 'integer'), Column('value', 'string'))
 
-Conforming after a migration does nothing.
+Conforming after a migration keeps the same columns, but other information might
+be lost. For example column data types might be lost (sqlite migrations don't
+change data types, boolean columns might be interpretted as integers, etc.)
 >>> mydb.conform()
 >>> mydb.test_table
 Table(Column('extra', 'string'), Column('key', 'string'), Column('value', 'string'))
 
+It is always recommended to conform your database *before* defining columns.
+>>> mydb.test_table = Table(Column('key', 'integer'), Column('value'), Column('extra'))
+
+Conforming and migrating are both optional. Attempting to manipulate the
+database without these calls may fail if table definitions don't match tables
+in the database. However, conform unconditionally reads all tables, so it may
+not be appropriate for large databases. Be careful using migrate on databases
+that are shared between applications, as it can break those applications if
+a table is renamed or altered.
+
+===
+Data
+===
+
 Add some data by calling insert on a table. An integer referring to the new row
 is returned and can be used to retrieve it later.
->>> i = mydb.test_table.insert(key='1', value='a')
->>> row = mydb.test_table[i]
+>>> mydb = DB()
+>>> mydb.test_table = Table(Column('key', 'integer'), Column('value'))
+
+Insert adds a row to the table and returns its row id
+>>> mydb.test_table.insert(key='100', value='a')
+1
+>>> row = mydb.test_table[1]
 >>> row.key
-'1'
+100
 >>> row.value
 'a'
->>> print row.extra
-None
+>>> del mydb.test_table[1]
+
+===
+Consistency
+===
+
+The database acts as a context manager which controls data integrity. If several
+operations need to be treated atomically:
+
+If an error is raised, the all of the transactions are rolled back. Only the
+outer-most context manager commits the transaction. Individual calls that modify
+the database are wrapped in their own context managers, so they are committed
+automatically.
+>>> with mydb:
+...   mydb.test_table.insert(key=3, value='c')
+...   mydb.test_table.insert(key=4, value='d')
+...   raise Exception
+Traceback (most recent call last):
+ ...
+Exception
+>>> list(mydb.test_table.select())
+[]
+
+>>> with mydb:
+...   mydb.test_table.insert(key=3, value='c')
+...   mydb.test_table.insert(key=7, value='g')
+1
+2
+>>> for row in mydb.test_table.select():
+...   print sorted(row.items())
+[('key', 3), ('rowid', 1), ('value', 'c')]
+[('key', 7), ('rowid', 2), ('value', 'g')]
+
+===
+Querying
+===
+
+Doing comparison, binary or arithmetic operations on columns produces 'Where'
+objects.
+>>> mydb.test_table.key <= 3
+Where([LESSEQUAL, Column('key', 'integer'), 3])
+
+The resulting object can be queried. Standard SQL commands are provided. Using
+parentheses, a query can be set up and then selected:
+>>> for row in (mydb.test_table.key<=3).select():
+...   print sorted(row.items())
+[('key', 3), ('rowid', 1), ('value', 'c')]
+
+Rows in a query can be counted...
+>>> (mydb.test_table.key>1).count()
+2
+
+or updated...
+>>> (mydb.test_table.value=='c').update(key=4)
+>>> for row in mydb.test_table.select():
+...   print sorted(row.items())
+[('key', 4), ('rowid', 1), ('value', 'c')]
+[('key', 7), ('rowid', 2), ('value', 'g')]
+
+or deleted...
+>>> (mydb.test_table.key > 5).delete()
+>>> for row in mydb.test_table.select():
+...   print sorted(row.items())
+[('key', 4), ('rowid', 1), ('value', 'c')]
+
 
 """
 import collections
 import inspect
 import drivers.sqlite
+try:
+	import drivers.base
+except ImportError:
+	drivers.base = drivers.sqlite
 
-from webdoc.common import container
+from webdoc.common import container, flatten
 
-class collection(collections.MutableSet):
+class collection(collections.MutableSet, collections.MutableMapping):
 	'''Set of objects which can also be retrieved by name
 
 	>>> class b(object):
@@ -77,6 +168,9 @@ class collection(collections.MutableSet):
 	b('robert', 'Sys Admin')
 	>>> sorted(list(a), key=lambda x:x.name)
 	[b('josephine', 'Q/A'), b('robert', 'Sys Admin')]
+	>>> a['stephanie'] = b('robert', 'Sys Admin')
+	>>> a['stephanie']
+	b('stephanie', 'Sys Admin')
 	'''
 	def __init__(self, elements=(), namekey='name'):
 		self._key = namekey
@@ -102,6 +196,10 @@ class collection(collections.MutableSet):
 
 	def __getitem__(self, key):
 		return self._data[key]
+		
+	def __setitem__(self, key, value):
+		setattr(value, self._key, key)
+		self._data[key] = value
 
 	def __delitem__(self, key):
 		del self._data[key]
@@ -113,39 +211,138 @@ class collection(collections.MutableSet):
 		else:
 			return self._data.popitem()[1]
 
-class Row(object):
-	pass
-
 class Selection(object):
-	pass
+	def __init__(self, columns, values):
+		self.columns = columns
+		self.values = values
+		
+	def __iter__(self):
+		for value in self.values:
+			yield Row(self.columns, value)
+
+class Row(container):
+	def __init__(self, columns, values):
+		container.__init__(self, [(c.name,c.represent(v)) for v,c in zip(values,columns)])
+
+class Expression(object):
+	def __eq__(self, x):
+		return Where(self._db, [drivers.base.EQUAL, getattr(self,'_where_tree',self), x])
+	def __ne__(self, x):
+		return Where(self._db, [drivers.base.NOTEQUAL, getattr(self,'_where_tree',self), x])
+	def __le__(self, x):
+		return Where(self._db, [drivers.base.LESSEQUAL, getattr(self,'_where_tree',self), x])
+	def __ge__(self, x):
+		return Where(self._db, [drivers.base.GREATEREQUAL, getattr(self,'_where_tree',self), x])
+	def __lt__(self, x):
+		return Where(self._db, [drivers.base.LESSTHAN, getattr(self,'_where_tree',self), x])
+	def __gt__(self, x):
+		return Where(self._db, [drivers.base.GREATERTHAN, getattr(self,'_where_tree',self), x])
 	
-class Set(object):
-	pass
+	def __add__(self, x):
+		if isinstance(x, basestring) or \
+		self.type in ('string','text','data') or \
+		(isinstance(x, Column) and x.type in ('string','text','data')):
+			return Where(self._db, [drivers.base.CONCATENATE, getattr(self,'_where_tree',self), x])
+		else:
+			return Where(self._db, [drivers.base.ADD, getattr(self,'_where_tree',self), x])
+	def __sub__(self, x):
+		return Where(self._db, [drivers.base.SUBTRACT, getattr(self,'_where_tree',self), x])
+	def __mul__(self, x):
+		return Where(self._db, [drivers.base.MULTIPLY, getattr(self,'_where_tree',self), x])
+	def __div__(self, x):
+		return Where(self._db, [drivers.base.DIVIDE, getattr(self,'_where_tree',self), x])
+	def __floordiv__(self, x):
+		return Where(self._db, [drivers.base.FLOORDIVIDE, getattr(self,'_where_tree',self), x])
+	def __div__(self, x):
+		return Where(self._db, [drivers.base.DIVIDE, getattr(self,'_where_tree',self), x])
+	def __truediv__(self, x):
+		return Where(self._db, [drivers.base.DIVIDE, getattr(self,'_where_tree',self), x])
+	def __mod__(self, x):
+		return Where(self._db, [drivers.base.MODULO, getattr(self,'_where_tree',self), x])
+
+class Where(Expression):
+	def __init__(self, db, where_tree):
+		self._db = db
+		self._where_tree = where_tree
+
+	def _get_columns(self, columns):
+		if not columns:
+			columns = [table.ALL for table in self._get_tables()]
+		return flatten(columns)
+		
+	def _get_tables(self):
+		tables = set()
+		for entity in flatten(self._where_tree):
+			if isinstance(entity, Column):
+				tables.add(entity.table)
+		return tables
+		
+	def select(self, *columns):
+		columns = self._get_columns(columns)
+		values = self._db.__driver__.select(columns, self._where_tree)
+		return Selection(columns, values)
+		
+	def select_one(self, *columns):
+		columns = self._get_columns(columns)
+		values = self._db.__driver__.select(columns, self._where_tree)
+		return Row(columns, values.fetchone())
+		
+	def count(self):
+		columns = [table.rowid for table in self._get_tables()]
+		values = self._db.__driver__.select(columns, self._where_tree)
+		return len(values.fetchall())
+		
+	def update(self, **values):
+		self._db.__driver__.update(self._get_tables().pop()._name, self._where_tree, values)
+		
+	def delete(self):
+		self._db.__driver__.delete(self._get_tables().pop()._name, self._where_tree)
+		
+	def __repr__(self):
+		return 'Where(%r)'%self._where_tree
 
 ident = lambda x:x
 
-class Column(object):
+class Column(Expression):
 	'''Column(name, type='string', notnull=False, default=None)
 	
 	valid types:
+	'rowid'
 	'string' or basestring
 	'text'
 	'integer' or int
+	'float' or float
 	'boolean' or bool
+	'data' or bytes
 	'''
-	def __init__(self, name, type='string', notnull=False, default=None):
+	def __init__(self, name, datatype='string', notnull=False, default=None):
 		self.name = name
-		if type == 'string':
-			self.type = 'string'
-			self.interpret = str
-			self.represent = str
+		if isinstance(datatype, type):
+			self.interpret = self.represent = datatype
+			sample = datatype()
+			if isinstance(sample, basestring):
+				self.type = 'string'
+			elif isinstance(sample, bool):
+				self.type = 'boolean'
+			elif isinstance(sample, int):
+				self.type = 'integer'
+			elif isinstance(sample, float):
+				self.type = 'float'
+			else:
+				self.type = 'data'
 		else:
-			self.type = type
-			self.interpret = ident
-			self.represent = ident
+			self.type = datatype
+			if datatype in ('string','text','data'):
+				self.interpret = self.represent = str
+			elif datatype in ('integer','rowid'):
+				self.interpret = self.represent = int
+			elif type == 'float':
+				self.interpret = self.represent = float
+			elif type == 'boolean':
+				self.interpret = self.represent = bool
+			
 		self.notnull = notnull
 		self.default = default
-		self.index = None
 		
 	def __repr__(self):
 		values = [`self.name`]
@@ -156,30 +353,54 @@ class Column(object):
 			values.append('default=%r' % self.default)
 		return 'Column(%s)' % ', '.join(values)
 		
-	def __cmp__(self, x):
-		return cmp((self.name,vars(self)), (x.name,vars(self))) if x else 1
-
+	@property
+	def _db(self):
+		return self.table._db
+	
 class Table(collection):
 	def __init__(self, *columns, **kwargs):
 		collection.__init__(self, columns)
+		for c in columns:
+			c.table = self
+
+	@property
+	def rowid(self):
+		c = Column('rowid', 'rowid')
+		c.table = self
+		return c
+		
+	@property
+	def ALL(self):
+		return [self.rowid]+list(self)
 
 	def __getattr__(self, key):
 		return collection.__getitem__(self, key)
 
 	def __delattr__(self, key):
 		collection.__delitem__(self, key)
+		
+	def __hash__(self):
+		return hash(self._name)
 
 	def __getitem__(self, key):
-		names = self.keys()
-		columns = list(self)
-		values = self._db.__driver__.select(names, self._name, [('rowid',key)])[0]
-		return container((k,c.represent(v)) for k,v,c in zip(['rowid']+names,values,columns))
+		try:
+			key = int(key)
+		except ValueError:
+			raise TypeError("rowid's must be integers")
+		columns = self.ALL
+		value = (self.rowid==key).select_one(columns)
+		if not value:
+			raise KeyError("No row with rowid %i" % key)
+		return value
 
 	def __delitem__(self, key):
-		pass
+		(self.rowid==key).delete()
 
 	def insert(self, **values):
 		return self._db.__driver__.insert(values, self._name)
+
+	def select(self, *columns):
+		return Where(self._db, None).select(*(columns or self.ALL))
 
 	def __repr__(self):
 		return 'Table(%s)' % ', '.join(sorted(map(repr,self)))
@@ -196,13 +417,18 @@ class DB(collection):
 
 	def __init__(self):
 		collection.__init__(self, namekey='_name')
+		
+	def __enter__(self):
+		self.__driver__.__enter__()
+		
+	def __exit__(self, obj, exc, tb):
+		self.__driver__.__exit__(obj, exc, tb)
 	
 	def __setitem__(self, key, value):
 		assert isinstance(value, Table)
 		value._db = self
-		value._name = key
 		self.__driver__.create_table_if_nexists(key, value)
-		collection.add(self, value)
+		collection.__setitem__(self, key, value)
 
 	def __setattr__(self, key, value):
 		if key[0] == '_':
@@ -221,6 +447,9 @@ class DB(collection):
 			super(DB, self).__delattr__(key)
 		else:
 			del self[key]
+
+	def where(self, *conditions):
+		return reduce(lambda x,y:x&y, conditions)
 
 	@classmethod
 	def connect(cls, name, *args, **kwargs):
--- webdb/drivers/sqlite.py
+++ webdb/drivers/sqlite.py
@@ -1,25 +1,55 @@
 
 from webdoc import container
 import sqlite3
+import errno
+
+class ColumnError(Exception): pass
+
+class op(object):
+	def __repr__(self):
+		return self.__class__.__name__
+	def __eq__(self, x):
+		return isinstance(x, self.__class__)
+class EQUAL(op): pass
+EQUAL = EQUAL()
+class NOTEQUAL(op): pass
+NOTEQUAL = NOTEQUAL()
+class LESSTHAN(op): pass
+LESSTHAN = LESSTHAN()
+class LESSEQUAL(op): pass
+LESSEQUAL = LESSEQUAL()
+class GREATERTHAN(op): pass
+GREATERTHAN = GREATERTHAN()
+class GREATEREQUAL(op): pass
+GREATEREQUAL = GREATEREQUAL()
+class ADD(op): pass
+ADD = ADD()
+class CONCATENATE(op): pass
+CONCATENATE = CONCATENATE()
+class SUBTRACT(op): pass
+SUBTRACT = SUBTRACT()
+class MULTIPLY(op): pass
+MULTIPLY = MULTIPLY()
+class DIVIDE(op): pass
+DIVIDE = DIVIDE()
+class FLOORDIVIDE(op): pass
+FLOORDIVIDE = FLOORDIVIDE()
+class MODULO(op): pass
+MODULO = MODULO()
+class AND(op): pass
+class OR(op): pass
+class NOT(op): pass
 
 class driver_base(object):
+	def __init__(self, connection):
+		self.connection = connection
+		self.depth = 0
+		self.cursor = None
+
 	def create_table_if_nexists(self, name, table):
 		if name not in self.list_tables:
 			self.create_table(name, table)
 
-class sqlite(object):
-	"""Driver for sqlite3 databases
-
-	sqlite accepts only one parameter: path, which is the path of the database
-	file. By default, path=':memory:', which creates a temporary database
-	in memory.
-	"""
-	def __init__(self, path=':memory:'):
-		self.path = path
-		self.connection = sqlite3.connect(path)
-		self.depth = 0
-		self.cursor = None
-		
 	def __enter__(self):
 		self.depth += 1
 		if self.cursor is None:
@@ -34,67 +64,187 @@ class sqlite(object):
 			else:
 				self.connection.commit()
 			self.cursor = None
-
+		
 	def execute(self, sql, values=()):
+		self.lastsql = sql
 		with self as cursor:
 			return cursor.execute(sql, values)
+		
+	def validate_name(self, name):
+		if not name.replace('_','').isalnum():
+			raise ColumnError("Column names can only contain letters, numbers, and underscores. Got %r" % name)
+
+class sqlite(driver_base):
+	"""Driver for sqlite3 databases
+
+	sqlite accepts only one parameter: path, which is the path of the database
+	file. By default, path=':memory:', which creates a temporary database
+	in memory.
+	"""
+	def __init__(self, path=':memory:'):
+		self.path = path
+		try:
+			driver_base.__init__(self, sqlite3.connect(path))
+		except sqlite3.OperationalError, e:
+			if e.message == 'unable to open database file':
+				e = IOError(errno.ENOENT, 'No such file or directory: %r' % path)
+				e.errno = errno.ENOENT
+			raise e
+			
+	operators = {
+		EQUAL:lambda a,b:'%s=%s'%(a,b),
+		LESSEQUAL:lambda a,b:'%s<=%s'%(a,b),
+		GREATERTHAN:lambda a,b:'%s>%s'%(a,b),
+	}
+	def protect_identifier(self, name):
+		return '"%s"'%name
+		
+	def represent_literal(self, value):
+		if isinstance(value, basestring):
+			return "'%s'"%value.replace("'", "\'")
+		else:
+			return repr(value)
+			
+	def column_name(self, table, col):
+		return '.'.join(map(self.protect_identifier, (table, col)))
+		
+	def parse_where(self, conditions):
+		if isinstance(conditions, list):
+			operator = conditions[0]
+			return '(%s)'%self.operators[operator](*map(self.parse_where,conditions[1:]))
+		elif hasattr(conditions, 'table') and hasattr(conditions, 'name'): #Column duck-typed
+			return self.column_name(conditions.table._name, conditions.name)
+		else:
+			return self.represent_literal(conditions)
 		
 	def list_tables(self):
 		return (str(n) for (n,) in self.execute("""SELECT name FROM sqlite_master WHERE type='table'"""))
 		
 	def list_columns(self, table):
-		return ((str(name),str(v_type),bool(notnull),default) for (_,name,v_type,notnull,default,_) in self.execute("""PRAGMA table_info("%s");""" % table))
+		return ((str(name),self.unmap_type(v_type),bool(notnull),default) for (_,name,v_type,notnull,default,_) in self.execute("""PRAGMA table_info("%s");""" % table))
 			
-	@staticmethod
-	def format_value(val):
+	def format_value(val, cast=None):
 		if val is None:
 			return 'NULL'
+		elif cast in ('INT','REAL'):
+			return "%g"%val
+		elif cast in ('TEXT','BLOB'):
+			return "'%s'"%str(val).replace("'", "''")
 		else:
-			return str(val)
+			return val
+			
+	def map_type(self, t):
+		if t == 'rowid':
+			return 'INTEGER PRIMARY KEY'
+		elif t == 'string':
+			return 'TEXT'
+		elif t == 'text':
+			return 'TEXT'
+		elif t == 'integer':
+			return 'INT'
+		elif t == 'float':
+			return 'REAL'
+		elif t == 'data':
+			return 'BLOB'
+		elif t == 'boolean':
+			return 'INT'
+			
+	def unmap_type(self, t):
+		if t == 'TEXT':
+			return 'string'
+		elif t == 'INT':
+			return 'integer'
+		elif t == 'REAL':
+			return 'float'
+		elif t == 'BLOB':
+			return 'data'
 
-	@classmethod
-	def format_column(cls, column):
+	def format_column(self, column):
 		props = container(vars(column))
-		props.notnull = 'NOT NULL' if column.notnull else ''
-		props.default = "DEFAULT '%s'"%cls.format_value(column.default) if column.notnull or not column.default is None else ''
-		return '%(name)s %(type)s %(notnull)s %(default)s' % props
+		self.validate_name(props.name)
+		props.type = self.map_type(props.type)
+		props.notnull = ' NOT NULL' if column.notnull else ''
+		props.default = " DEFAULT %s"%self.format_value(column.default, props.type) if column.notnull or not column.default is None else ''
+		return '"%(name)s" %(type)s%(notnull)s%(default)s' % props
 
 	def create_table_if_nexists(self, name, table):
-		self.execute("""CREATE TABLE IF NOT EXISTS %s(%s);""" % (
+		self.validate_name(name)
+		self.execute("""CREATE TABLE IF NOT EXISTS "%s"(%s);""" % (
 			name,
 			', '.join(map(self.format_column, table))
 		))
 
 	def create_table(self, name, table):
-		self.execute("""CREATE TABLE %s(%s);""" % (
+		self.validate_name(name)
+		self.execute("""CREATE TABLE "%s"(%s);""" % (
 			name,
 			', '.join(map(self.format_column,table))
 		))
 
 	def rename_table(self, orig, new):
-		self.execute("""ALTER TABLE %s RENAME TO %s;""" % (orig, new))
+		self.validate_name(orig)
+		self.validate_name(new)
+		self.execute("""ALTER TABLE "%s" RENAME TO "%s";""" % (orig, new))
 
 	def alter_table(self, name, table):
+		self.validate_name(name)
 		with self:
 			db_cols = self.list_columns(name)
 			db_names = [c[0] for c in db_cols]
 			for column in table:
 				if column.name not in db_names:
-					self.execute("""ALTER TABLE %s ADD COLUMN %s;"""%(
+					self.execute("""ALTER TABLE "%s" ADD COLUMN %s;"""%(
 						name, self.format_column(column)
 					))
 
-	def select(self, columns, table, conditions):
-		return self.execute("""SELECT %s FROM %s WHERE %s""" % (
-			', '.join(['rowid'] + columns),
-			table,
-			' and '.join("%s=%s"%i for i in conditions),
-		)).fetchall()
+	def select(self, columns, conditions):
+		tables = set()
+		names = []
+		for c in columns:
+			tables.add(c.table._name)
+			names.append((c.table._name,c.name))
+		where = self.parse_where(conditions) if conditions else ''
+		if where:
+			where = ' WHERE '+where
+		if len(tables) == 1:
+			table = tables.pop()
+			self.validate_name(table)
+			#where = ' and '.join("%s.%s=%s"%(c.table._name,c.name,v) for (c,v) in conditions)
+			sql = """SELECT %s FROM %s%s;""" % (
+				', '.join(self.column_name(*i) for i in names),
+				self.protect_identifier(table),
+				where,
+			)
+			return self.execute(sql)
+		else:
+			raise Exception("Can't handle query with %i tables: %s" % (len(tables), where))
 
 	def insert(self, values, table):
+		self.validate_name(table)
 		cur = self.execute("""INSERT INTO %s(%s) VALUES (%s)""" % (
-			table,
+			self.protect_identifier(table),
 			','.join(values.keys()),
 			','.join(list('?'*len(values))),
 		), values.values())
 		return cur.lastrowid
+
+	def update(self, table, conditions, values):
+		where = self.parse_where(conditions) if conditions else ''
+		if where:
+			where = ' WHERE '+where
+		sql = """UPDATE %s SET %s%s;""" % (
+			self.protect_identifier(table),
+			', '.join('%s=?'%(self.protect_identifier(k)) for k in values.keys()),
+			where,
+		)
+		return self.execute(sql, values.values())
+
+	def delete(self, table, conditions):
+		where = self.parse_where(conditions) if conditions else ''
+		if where:
+			where = ' WHERE '+where
+		sql = """DELETE FROM %s%s;""" % (
+			self.protect_identifier(table),
+			where,
+		)
+		return self.execute(sql)
