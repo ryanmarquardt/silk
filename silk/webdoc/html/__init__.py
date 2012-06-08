d7d5e0720bd788cf04204b6b0fa60c21f0d18fce
Ryan Marquardt <ryan.marquardt@gmail.com>
2012-06-08 12:34:04 -0700

    Opened up html sub-package

---
+++ silk/webdoc/html/__init__.py
@@ -0,0 +1,10 @@
+
+from .common import *
+
+if __name__=='__main__':
+	import doctest
+	doctest.testmod()
+
+	import html5
+	doctest.testmod(html5)
+	
---
+++ silk/webdoc/html/html5.py
@@ -0,0 +1,20 @@
+
+"""Module containing only html v5 tags. All deprecated tags have been removed.
+"""
+
+from .common import *
+
+del ACRONYM
+del APPLET
+del BASEFONT
+del BIG
+del CENTER
+del DIR
+del FONT
+del FRAME
+del FRAMESET
+del ISINDEX
+del NOFRAMES
+del STRIKE
+del TT
+del U
