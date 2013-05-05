e13203aabf7c4d6ce24551832bc1352264cebf02
Ryan Marquardt <ryan.marquardt@gmail.com>
2013-05-04 19:51:43 -0700

    Added module silk.globals

---
+++ silk/globals.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+try:
+	from silk.webdoc import *
+except ImportError:
+	pass
+
+try:
+	from silk.webdb import *
+except ImportError:
+	pass
+
+try:
+	from silk.webreq import *
+except ImportError:
+	pass
