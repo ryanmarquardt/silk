c36cce244b3eb54f3012f8a5e053316bf8db7e39
Ryan Marquardt <ryan.marquardt@gmail.com>
2013-06-18 23:17:37 -0700

    doctest/testwebdbdrivers.py: Added comments with mysql test setup instructions

--- doctest/testwebdbdrivers.py
+++ doctest/testwebdbdrivers.py
@@ -11,3 +11,7 @@ for driver in drivers.__all__:
 	conn = ','.join(map(repr,(driver,)+getattr(getattr(drivers,driver),driver).test_args))
 	o = type('',(object,),{'__doc__':fdoc%dict(conn=conn)})
 	doctest.run_docstring_examples(o, globals(), name=__file__+'(%s)'%driver)
+
+#If tests fail for mysql, run the following SQL:
+#  CREATE DATABASE silk_test;
+#  GRANT ALL PRIVILEGES ON SILK_TEST.* TO silk_test@localhost;
