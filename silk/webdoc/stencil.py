aab54fd4ce523a0d1e2c7e9a1a61f2a403261ea5
Ryan Marquardt <ryan.marquardt@gmail.com>
2013-07-22 15:16:24 -0700

    webdoc.stencil: Implemented extend and include

--- silk/webdoc/stencil.py
+++ silk/webdoc/stencil.py
@@ -38,8 +38,8 @@ class StencilFile(object):
 
 	def __iter__(self):
 		insub, multiline = False, ''
-		for lineno, line in enumerate(self.file, 1):
-			multiline, line, text, column = '', multiline+line, line, 0
+		for self.lineno, line in enumerate(self.file, 1):
+			multiline, line, text, self.column = '', multiline+line, line, 0
 			while line:
 				split = self.closer if insub else self.opener
 				token, found, line = line.partition(split)
@@ -49,32 +49,20 @@ class StencilFile(object):
 				if insub:
 					token = token.strip()
 				if token:
-					yield lineno, column, insub, token, text
-				column += len(token) + len(split)
+					yield insub, token, text
+				self.column += len(token) + len(split)
 				insub = not insub
 		if multiline or insub:
 			if insub:
-				raise SyntaxError('Unclosed substitution', (self.filename,lineno,column,text))
+				self.syntax_error('Unclosed substitution', text)
 			else:
-				yield lineno, column, insub, multiline, text
+				yield insub, multiline, text
 
-class Stencil(object):
+	def syntax_error(self, message, code, offset=0):
+		raise SyntaxError(message, (self.filename, self.lineno, self.column+offset, code))
+
+class BaseStencil(object):
 	"""
-	>>> erb = Stencil('<%', '%>')
-	>>> erb.compile('b.stencil', out=sys.stdout)
-	# extend 'a.stencil'
-	sys.stdout.write('abc\\n')
-	# include ''
-	sys.stdout.write('\\ndef\\n')
-	sys.stdout.write('\\nghi\\n')
-
-	>>> exec erb.compile('b.stencil')
-	abc
-	<BLANKLINE>
-	def
-	<BLANKLINE>
-	ghi
-
 	>>> parse = Stencil('{{', '}}', '  ').parse
 	>>> parse("{{=123}}")
 	'sys.stdout.write(str(123))\\n'
@@ -83,6 +71,8 @@ class Stencil(object):
 	sys.stdout.write('123')
 	if True:
 	  sys.stdout.write('456')
+
+	>>> erb = Stencil('<%', '%>')
 
 	>>> erb.parse("You have <%= 'no' if x==0 else x %> messages", out=sys.stdout)
 	sys.stdout.write('You have ')
@@ -116,6 +106,12 @@ class Stencil(object):
 	  if b:
 	    if c:
 	      sys.stdout.write(str(True))
+	
+	#>>> parse("{{if a:}}{{if b:}}{{if c:\\n print 123}}{{pass}}{{pass}}", out=sys.stdout)
+	#if a:
+	#  if b:
+	#    if c:
+	#      sys.stdout.write(str(123))
 
 	>>> parse('{{pass}}')
 	Traceback (most recent call last):
@@ -134,8 +130,6 @@ class Stencil(object):
 		self.indent = indent
 		self.subs = {
 			'=\s*':self.on_equal_sign,
-			'extend$|extend\s+':self.on_extend,
-			'include$|include\s+':self.on_include,
 		}
 
 	def is_dedent(self, token):
@@ -144,58 +138,39 @@ class Stencil(object):
 	def on_equal_sign(self, printer, token, remainder):
 		printer('%s(str(%s))' % (self.writer, remainder))
 
-	def open(self, path):
-		return __builtins__.open(path, 'r')
+	def check_syntax(self, stub):
+		if stub[-1] == ':':
+			if stub[:2] == 'el':
+				stub = 'if 1:pass\n'+stub
+			elif any(map(stub.startswith, ('except','finally'))):
+				stub = 'try:pass\n'+stub
+			stub += '\n\tpass'
+		try:
+			compile(stub, self.source.filename, 'exec')
+		except SyntaxError, err:
+			self.source.syntax_error(err.msg, orig, err.offset)
 
-	def on_extend(self, printer, token, remainder):
-		printer('# extend %r' % remainder)
-		this = self.sources.pop(-1)
-		self.sources.append(StencilFile(self.opener, self.closer, self.open(remainder), remainder))
-		self.sources[-1].extends = this
-
-	def on_include(self, printer, token, remainder):
-		printer('# include %r' % remainder)
-		if remainder:
-			self.sources.append(StencilFile(self.opener, self.closer, self.open(remainder), remainder))
-		elif hasattr(self.sources[-1], 'extends'):
-			this = self.sources[-1]
-			self.sources.append(this.extends)
-			del this.extends
-
-	def interpret(self, iprint, lineno, column, python, text, orig):
-		filename = self.sources[-1].filename
+	def interpret(self, iprint, python, text, orig):
 		for pattern, function in self.subs.items():
 			match = re.match('(%s)' % pattern, text)
 			if match:
-				column += match.end()
-				text = text[match.end():]
-				function(iprint, *(match.groups()+(text,)))
-				break
-		else:
+				self.source.column += match.end()
+				function(iprint, *(match.groups()+(text[match.end():],)))
+				return
+		if python:
 			dedent = self.is_dedent(text)
 			if dedent or any(text.startswith(b) for b in ('elif', 'else', 'except', 'finally')):
 				try:
 					iprint(DEDENT)
 				except ValueError:
-					raise SyntaxError('Got dedent outside of block', (filename, lineno, column, orig))
-			stub = text
-			if stub[-1] == ':':
-				if stub[:2] == 'el':
-					stub = 'if 1:pass\n'+stub
-				elif any(map(stub.startswith, ('except','finally'))):
-					stub = 'try:pass\n'+stub
-				stub += '\n\tpass'
-			try:
-				compile(stub, filename, 'exec')
-			except SyntaxError, err:
-				raise SyntaxError(err.msg, (filename, lineno, column+err.offset, orig))
+					self.source.syntax_error('Got dedent outside of block', orig)
 			if not dedent:
-				if python:
-					iprint(text)
-					if text[-1] == ':':
-						iprint(INDENT)
-				else:
-					iprint('%s(%r)' % (self.writer,text))
+				self.check_syntax(text)
+				iprint(text)
+				if text[-1] == ':':
+					iprint(INDENT)
+		else:
+			iprint('%s(%r)' % (self.writer,text))
 
 	def sequence(self):
 		while self.sources:
@@ -204,11 +179,15 @@ class Stencil(object):
 			except StopIteration:
 				del self.sources[-1]
 				continue
-			if values[3]:
+			if values[1]:
 				yield values
 
 	def parse(self, data, out=None):
 		return self.compile(StringIO(data), out=out)
+
+	@property
+	def source(self):
+		return self.sources[-1]
 
 	def compile(self, path_or_file, filename='<string>', out=None):
 		self.sources = [StencilFile(self.opener, self.closer, path_or_file, filename)]
@@ -216,10 +195,75 @@ class Stencil(object):
 		if _return:
 			out = StringIO()
 		iprint = IndentingPrinter(out, self.indent)
-		for lineno, col, python, text, orig in self.sequence():
-			self.interpret(iprint, lineno, col, python, text, orig)
+		for is_python, text, orig in self.sequence():
+			self.interpret(iprint, is_python, text, orig)
 		if _return:
 			return out.getvalue()
+
+class Stencil(BaseStencil):
+	"""subclass of BaseStencil which adds extending and including
+
+	Stencil uses ``open`` to get files for extension.
+
+	>>> class ERB(Stencil):
+	...   opener, closer = '<%', '%>'
+	...   def open(self, path):
+	...     return StringIO("abc\\n<% include %>\\nghi\\n")
+	
+	>>> erb = ERB()
+	
+	>>> erb.parse("<% extend a.stencil %>\\ndef\\n", out=sys.stdout)
+	sys.stdout.write('abc\\n')
+	sys.stdout.write('\\ndef\\n')
+	sys.stdout.write('\\nghi\\n')
+
+
+
+	>>> exec erb.parse("<% extend a.stencil %>\\ndef\\n")
+	abc
+	<BLANKLINE>
+	def
+	<BLANKLINE>
+	ghi
+
+	If there is no argument and the stencil is not being extended,
+	``include`` is a no-op.
+	
+	>>> erb.parse("<% include %>")
+	''
+
+	The parser only checks whole words, case sensitively
+
+	>>> erb.parse("<% includex %>")
+	'includex\\n'
+
+	>>> erb.parse("<% Include %>")
+	'Include\\n'
+	"""
+
+	def __init__(self, *args, **kwargs):
+		super(Stencil, self).__init__(*args, **kwargs)
+		self.subs.update({
+			'extend$|extend\s+':self.on_extend,
+			'include$|include\s+':self.on_include,
+		})
+
+	def open(self, path):
+		return __builtins__.open(path, 'r')
+
+	def on_extend(self, printer, token, remainder):
+		this = self.sources.pop(-1)
+		self.sources.append(StencilFile(self.opener, self.closer, self.open(remainder), remainder))
+		self.sources[-1].extends = this
+
+	def on_include(self, printer, token, remainder):
+		if remainder:
+			self.sources.append(StencilFile(self.opener, self.closer, self.open(remainder), remainder))
+		elif hasattr(self.sources[-1], 'extends'):
+			this = self.sources[-1]
+			self.sources.append(this.extends)
+			del this.extends
+
 
 if __name__ == '__main__':
 	import doctest
