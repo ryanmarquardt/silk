62f3e5361c7de979c577fb7a05e9ae67a473a3cf
Ryan Marquardt <ryan.marquardt@gmail.com>
2015-12-26 19:10:38 -0800

    Conform webreq.uri to flake8

--- silk/webreq/uri.py
+++ silk/webreq/uri.py
@@ -4,41 +4,45 @@ from urllib.parse import urlsplit, urlunsplit
 from urllib.parse import urlencode
 from wsgiref.util import request_uri
 
-from .. import MultiDict
 from .query import Query
 
+
 class URI(object):
-	def __init__(self, uri='', scheme='', host='', path=None, query=None, anchor=''):
-		self.scheme, self.host, self.path, self.query, self.anchor = urlsplit(uri, scheme='http')
-		self.scheme = scheme or self.scheme
-		self.host = host or self.host
-		self.path = path or self.path or '/'
-		self.query = query or self.query or Query()
-		self.anchor = anchor or self.anchor
+    def __init__(self, uri='', scheme='', host='', path=None, query=None,
+                 anchor=''):
+        self.scheme, self.host, self.path, self.query, self.anchor = \
+            urlsplit(uri, scheme='http')
+        self.scheme = scheme or self.scheme
+        self.host = host or self.host
+        self.path = path or self.path or '/'
+        self.query = query or self.query or Query()
+        self.anchor = anchor or self.anchor
 
-	@property
-	def query(self):
-		return self.__dict__['query']
-	@query.setter
-	def query(self, new):
-		if isinstance(new, str):
-			new = Query.parse(new)
-		elif not isinstance(new, Query):
-			new = Query(new)
-		self.__dict__['query'] = new
+    @property
+    def query(self):
+        return self.__dict__['query']
 
-	def get_path(self):
-		return '/'.join(['']+self.path)
+    @query.setter
+    def query(self, new):
+        if isinstance(new, str):
+            new = Query.parse(new)
+        elif not isinstance(new, Query):
+            new = Query(new)
+        self.__dict__['query'] = new
 
-	def get_query(self):
-		return urlencode(self.query)
+    def get_path(self):
+        return '/'.join(['']+self.path)
 
-	@classmethod
-	def from_env(cls, env):
-		return cls(request_uri(env))
+    def get_query(self):
+        return urlencode(self.query)
 
-	def __str__(self):
-		return urlunsplit((self.scheme, self.host, self.path, self.get_query(), self.anchor))
+    @classmethod
+    def from_env(cls, env):
+        return cls(request_uri(env))
 
-	def __repr__(self):
-		return 'URI(%r)' % (str(self),)
+    def __str__(self):
+        return urlunsplit((self.scheme, self.host, self.path,
+                           self.get_query(), self.anchor))
+
+    def __repr__(self):
+        return 'URI(%r)' % (str(self),)
