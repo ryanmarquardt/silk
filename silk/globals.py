e93789084c03b2a566835006d6d5adaee3d4bbe6
Ryan Marquardt <ryan.marquardt@gmail.com>
2015-12-26 19:59:08 -0800

    Use += to modify __all__, to appease flake8

--- silk/globals.py
+++ silk/globals.py
@@ -4,7 +4,7 @@ __all__ = []
 
 try:
     from silk.webdoc import css, html, node
-    __all__.extend(('css', 'html', 'node'))
+    __all__ += ['css', 'html', 'node']
 except ImportError:
     pass
 
@@ -14,12 +14,12 @@ try:
         DateTimeColumn, FloatColumn, IntColumn, RecordError, ReferenceColumn,
         RowidColumn, SQLSyntaxError, StrColumn, Table, UnknownDriver, connect
     )
-    __all__.extend((
+    __all__ += [
         'AuthenticationError', 'BoolColumn', 'Column', 'DB', 'DataColumn',
         'DateTimeColumn', 'FloatColumn', 'IntColumn', 'RecordError',
         'ReferenceColumn', 'RowidColumn', 'SQLSyntaxError', 'StrColumn',
         'Table', 'UnknownDriver', 'connect'
-    ))
+    ]
 except ImportError:
     pass
 
@@ -29,10 +29,10 @@ try:
         PathRouter, Query, Redirect, Response, TextView, URI
     )
 
-    __all__.extend((
+    __all__ += [
         'B64Document', 'BaseRouter', 'Document', 'FormData', 'HTTP', 'Header',
         'HeaderList', 'PathRouter', 'Query', 'Redirect', 'Response',
         'TextView', 'URI'
-    ))
+    ]
 except ImportError:
     pass
