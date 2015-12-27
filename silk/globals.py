0cdddec774d56bbb20a3e8046a40fe99d952a2f5
Ryan Marquardt <ryan.marquardt@gmail.com>
2015-12-26 19:57:39 -0800

    Make * imports explicit

--- silk/globals.py
+++ silk/globals.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python
 
-try:
-	from silk.webdoc import *
-except ImportError:
-	pass
+__all__ = []
 
 try:
-	from silk.webdb import *
+    from silk.webdoc import css, html, node
+    __all__.extend(('css', 'html', 'node'))
 except ImportError:
-	pass
+    pass
 
 try:
-	from silk.webreq import *
+    from silk.webdb import (
+        AuthenticationError, BoolColumn, Column, DB, DataColumn,
+        DateTimeColumn, FloatColumn, IntColumn, RecordError, ReferenceColumn,
+        RowidColumn, SQLSyntaxError, StrColumn, Table, UnknownDriver, connect
+    )
+    __all__.extend((
+        'AuthenticationError', 'BoolColumn', 'Column', 'DB', 'DataColumn',
+        'DateTimeColumn', 'FloatColumn', 'IntColumn', 'RecordError',
+        'ReferenceColumn', 'RowidColumn', 'SQLSyntaxError', 'StrColumn',
+        'Table', 'UnknownDriver', 'connect'
+    ))
 except ImportError:
-	pass
+    pass
+
+try:
+    from silk.webreq import (
+        B64Document, BaseRouter, Document, FormData, HTTP, Header, HeaderList,
+        PathRouter, Query, Redirect, Response, TextView, URI
+    )
+
+    __all__.extend((
+        'B64Document', 'BaseRouter', 'Document', 'FormData', 'HTTP', 'Header',
+        'HeaderList', 'PathRouter', 'Query', 'Redirect', 'Response',
+        'TextView', 'URI'
+    ))
+except ImportError:
+    pass
