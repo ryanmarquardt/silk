b85f9088d77a73480b7e7971e4107b11fd8af603
Ryan Marquardt <ryan.marquardt@gmail.com>
2015-12-26 19:11:42 -0800

    Conform webreq.query to flake8

--- silk/webreq/query.py
+++ silk/webreq/query.py
@@ -4,15 +4,17 @@ from urllib.parse import parse_qsl
 
 from .. import MultiDict
 
-class Query(MultiDict):
-	"""
 
-	>>> q = Query.parse('a=1&b=2&a=3')
-	>>> sorted(q.items())
-	[('a', '1'), ('a', '3'), ('b', '2')]
-	"""
-	keep_blank_values = True
-	strict_parsing = False
-	@classmethod
-	def parse(cls, line):
-		return cls(parse_qsl(line, cls.keep_blank_values, cls.strict_parsing))
+class Query(MultiDict):
+    """
+
+    >>> q = Query.parse('a=1&b=2&a=3')
+    >>> sorted(q.items())
+    [('a', '1'), ('a', '3'), ('b', '2')]
+    """
+    keep_blank_values = True
+    strict_parsing = False
+
+    @classmethod
+    def parse(cls, line):
+        return cls(parse_qsl(line, cls.keep_blank_values, cls.strict_parsing))
