34814ad24966c3ea6a91502b12195efef8e46694
Ryan Marquardt <ryan.marquardt@gmail.com>
2015-12-26 18:59:20 -0800

    Squash flake8 complaint by setting __all__

--- silk/webdoc/html/__init__.py
+++ silk/webdoc/html/__init__.py
@@ -1,5 +1,5 @@
 
-from .common import (  # noqa
+from .common import (
     A,
     ABBR,
     ACRONYM,
@@ -138,3 +138,23 @@ from .common import (  # noqa
     xmlescape,
     xmlunescape,
 )
+
+__all__ = [
+    'A', 'ABBR', 'ACRONYM', 'ADDRESS', 'APPLET', 'AREA', 'ARTICLE', 'ASIDE',
+    'AUDIO', 'B', 'BASE', 'BASEFONT', 'BDI', 'BDO', 'BIG', 'BLOCKQUOTE',
+    'BODY', 'BR', 'BUTTON', 'Body', 'CANVAS', 'CAPTION', 'CAT', 'CENTER',
+    'CITE', 'CODE', 'COL', 'COLGROUP', 'COMMENT', 'CONDITIONAL_COMMENT',
+    'DATALIST', 'DD', 'DEL', 'DETAILS', 'DFN', 'DIALOG', 'DIR', 'DIV', 'DL',
+    'DT', 'EM', 'EMBED', 'FIELDSET', 'FIGCAPTION', 'FIGURE', 'FONT', 'FOOTER',
+    'FORM', 'FRAME', 'FRAMESET', 'Form', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6',
+    'HEAD', 'HEADER', 'HR', 'HTML', 'HTMLDoc', 'Hyper', 'I', 'IFRAME', 'IMG',
+    'INPUT', 'INS', 'Image', 'Javascript', 'KBD', 'KEYGEN', 'LABEL', 'LEGEND',
+    'LI', 'LINK', 'MAIN', 'MAP', 'MARK', 'MENU', 'MENUITEM', 'META', 'METER',
+    'NAV', 'NBSP', 'NOFRAMES', 'NOSCRIPT', 'OBJECT', 'OL', 'OPTGROUP',
+    'OPTION', 'OUTPUT', 'P', 'PARAM', 'PRE', 'PROGRESS', 'Q', 'RP', 'RT',
+    'RUBY', 'S', 'SAMP', 'SCRIPT', 'SECTION', 'SELECT', 'SMALL', 'SOURCE',
+    'SPAN', 'STRIKE', 'STRONG', 'STYLE', 'SUB', 'SUMMARY', 'SUP', 'TABLE',
+    'TBODY', 'TD', 'TEXTAREA', 'TFOOT', 'TH', 'THEAD', 'TIME', 'TITLE', 'TR',
+    'TRACK', 'TT', 'U', 'UL', 'VAR', 'VIDEO', 'WBR', 'XML', 'XMLEntity',
+    'XMLNode', 'XMP', 'xmlescape', 'xmlunescape'
+]
