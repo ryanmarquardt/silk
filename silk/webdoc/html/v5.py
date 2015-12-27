1b09fb7cea11ce764abc811291782d05a8bb9611
Ryan Marquardt <ryan.marquardt@gmail.com>
2015-12-26 18:05:34 -0800

    Squash flake8 complaints by setting __all__

--- silk/webdoc/html/v5.py
+++ silk/webdoc/html/v5.py
@@ -3,7 +3,7 @@
 Module containing only html v5 tags. All deprecated tags have been removed.
 """
 
-from .common import (  # flake8: noqa
+from .common import (
     A,
     ABBR,
     # ACRONYM,
@@ -142,3 +142,22 @@ from .common import (  # flake8: noqa
     xmlescape,
     xmlunescape
 )
+
+__all__ = [
+    'A', 'ABBR', 'ADDRESS', 'AREA', 'ARTICLE', 'ASIDE', 'AUDIO', 'B', 'BASE',
+    'BDI', 'BDO', 'BLOCKQUOTE', 'BODY', 'BR', 'BUTTON', 'Body', 'CANVAS',
+    'CAPTION', 'CAT', 'CITE', 'CODE', 'COL', 'COLGROUP', 'COMMENT',
+    'CONDITIONAL_COMMENT', 'DATALIST', 'DD', 'DEL', 'DETAILS', 'DFN',
+    'DIALOG', 'DIV', 'DL', 'DT', 'EM', 'EMBED', 'FIELDSET', 'FIGCAPTION',
+    'FIGURE', 'FOOTER', 'FORM', 'Form', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6',
+    'HEAD', 'HEADER', 'HR', 'HTML', 'HTMLDoc', 'Hyper', 'I', 'IFRAME', 'IMG',
+    'INPUT', 'INS', 'Image', 'Javascript', 'KBD', 'KEYGEN', 'LABEL', 'LEGEND',
+    'LI', 'LINK', 'MAIN', 'MAP', 'MARK', 'MENU', 'MENUITEM', 'META', 'METER',
+    'NAV', 'NBSP', 'NOSCRIPT', 'OBJECT', 'OL', 'OPTGROUP', 'OPTION', 'OUTPUT',
+    'P', 'PARAM', 'PRE', 'PROGRESS', 'Q', 'RP', 'RT', 'RUBY', 'S', 'SAMP',
+    'SCRIPT', 'SECTION', 'SELECT', 'SMALL', 'SOURCE', 'SPAN', 'STRONG',
+    'STYLE', 'SUB', 'SUMMARY', 'SUP', 'TABLE', 'TBODY', 'TD', 'TEXTAREA',
+    'TFOOT', 'TH', 'THEAD', 'TIME', 'TITLE', 'TR', 'TRACK', 'U', 'UL', 'VAR',
+    'VIDEO', 'WBR', 'XML', 'XMLEntity', 'XMLNode', 'XMP', 'xmlescape',
+    'xmlunescape',
+]
